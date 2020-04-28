64aaca78fa0328c3b7329f34dee4124ffe90d053
ferueda <fe.rueda@outlook.com>
2020-04-27 22:46:37 -0400

    added controllers for searches with id

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -17,7 +17,37 @@ jobsRouter.post('/', async (req, res, next) => {
 jobsRouter.get('/', async (req, res, next) => {
   try {
     const jobs = await Job.find({});
+    console.log(jobs.length);
     res.json(jobs.map((job) => job.toJSON()));
+  } catch (e) {
+    next(e);
+  }
+});
+
+jobsRouter.get('/:id', async (req, res, next) => {
+  try {
+    const job = await Job.find({ indeed_id: req.params.id });
+
+    if (job.length) {
+      res.json(job.toJSON());
+    } else {
+      res.status(404).end();
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+jobsRouter.get('/title', async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const job = await Job.find({ job_title: req.body });
+
+    if (job.length) {
+      res.json(job.toJSON());
+    } else {
+      res.status(404).end();
+    }
   } catch (e) {
     next(e);
   }
